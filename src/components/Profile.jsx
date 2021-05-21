f844478cca55068c92bb9c2677fdf465c6cd4572
Vladimir_Amfik <e.v.e.n@mail.ru>
2021-05-21 11:48:29 +0300

    12-lesson

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+      <div className="content">
+      <div>
+        <img src="https://global-uploads.webflow.com/5ef5480befd392489dacf544/5f9f5e5943de7e69a1339242_5f44a7398c0cdf460857e744_img-image.jpeg"></img>
+      </div>
+      <div>
+        Ava + Description
+      </div>
+      <div>
+        My post
+        <div>
+          New post
+        </div>
+      </div>
+      <div>
+        <div>Post 1</div>
+        <div>Post 2</div>
+        <div>Post 3</div>
+      </div>
+    </div>
+    );
+}
+
+export default Profile;
