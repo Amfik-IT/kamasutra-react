2168460523962e5f0ba8b1240db3d392eff6adfd
Vladimir_Amfik <e.v.e.n@mail.ru>
2021-06-09 10:40:04 +0300

    29-lesson

---
+++ src/Redux/State.js
@@ -0,0 +1,28 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: "Hi? how are you", likeCount: 15, likeIcon: "&#9829;"},
+            {id: 1, message: "It's my firs post", likeCount: 20, likeIcon: "&#9829;"},
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: "Vova"},
+            {id: 2, name: "Dimych"},
+            {id: 3, name: "Slava"},
+            {id: 4, name: "Ilia"},
+            {id: 5, name: "Andrey"},
+            {id: 6, name: "Sasha"},
+        ],
+        messages: [
+            {id: 1, message: "Hi"},
+            {id: 2, message: "How is your it-kamasutra"},
+            {id: 3, message: "Yo"},
+            {id: 4, message: "Yo"},
+            {id: 5, message: "Yo"},
+        ],
+    },
+    sidebar: {},
+}
+
+export default state;
