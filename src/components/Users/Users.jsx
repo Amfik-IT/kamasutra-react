51cda553dcdbf8ea2cb1b822d280088b3125efea
Vladimir_Amfik <e.v.e.n@mail.ru>
2021-06-22 11:43:58 +0300

    52-lesson

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,17 +5,20 @@ import userPhoto from '../../assets/images/ava.png';
 
 let Users = (props) => {
 
-    if (props.users.length === 0) {
+    let getUsers = () => {
+        if (props.users.length === 0) {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-            props.setUsers(response.data.items)
-        });
-    }
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items)
+                });
+        }
+    };
 
 
     return (
         <div>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map(u => <div className={s.content} key={u.id}>
                     <div className={s.ava}>
